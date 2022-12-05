dbc39db8247d804d1052c6d2a3bcfaf8a156e7c1
John Durnall <john@industel.co.uk>
2022-12-05 14:16:32 +0000

    day5

---
+++ 2022/day5/Program.cs
@@ -0,0 +1,635 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.RegularExpressions;
+
+Console.WriteLine("Hello, World!");
+
+var puzzleInput = @"                [V]     [C]     [M]
+[V]     [J]     [N]     [H]     [V]
+[R] [F] [N]     [W]     [Z]     [N]
+[H] [R] [D]     [Q] [M] [L]     [B]
+[B] [C] [H] [V] [R] [C] [G]     [R]
+[G] [G] [F] [S] [D] [H] [B] [R] [S]
+[D] [N] [S] [D] [H] [G] [J] [J] [G]
+[W] [J] [L] [J] [S] [P] [F] [S] [L]
+ 1   2   3   4   5   6   7   8   9 
+
+move 2 from 2 to 7
+move 8 from 5 to 6
+move 2 from 4 to 5
+move 1 from 4 to 5
+move 1 from 5 to 8
+move 5 from 9 to 2
+move 7 from 1 to 6
+move 7 from 3 to 8
+move 1 from 4 to 6
+move 2 from 5 to 6
+move 6 from 7 to 5
+move 2 from 2 to 4
+move 4 from 5 to 2
+move 10 from 8 to 1
+move 2 from 7 to 4
+move 4 from 2 to 8
+move 2 from 9 to 8
+move 1 from 8 to 4
+move 2 from 4 to 9
+move 5 from 8 to 2
+move 1 from 4 to 6
+move 1 from 8 to 9
+move 1 from 7 to 2
+move 2 from 4 to 2
+move 1 from 7 to 3
+move 13 from 2 to 1
+move 1 from 2 to 4
+move 1 from 2 to 3
+move 2 from 5 to 4
+move 17 from 6 to 4
+move 3 from 4 to 9
+move 14 from 1 to 4
+move 4 from 6 to 8
+move 1 from 9 to 8
+move 23 from 4 to 8
+move 6 from 1 to 7
+move 3 from 1 to 5
+move 1 from 3 to 8
+move 5 from 7 to 8
+move 1 from 3 to 4
+move 1 from 5 to 3
+move 1 from 5 to 1
+move 1 from 3 to 2
+move 1 from 9 to 4
+move 9 from 4 to 9
+move 1 from 1 to 2
+move 11 from 8 to 2
+move 1 from 4 to 5
+move 13 from 2 to 3
+move 7 from 9 to 6
+move 1 from 5 to 6
+move 1 from 5 to 2
+move 1 from 9 to 4
+move 1 from 4 to 9
+move 2 from 8 to 9
+move 1 from 7 to 8
+move 8 from 9 to 1
+move 8 from 1 to 4
+move 4 from 6 to 7
+move 1 from 9 to 4
+move 2 from 3 to 9
+move 1 from 9 to 1
+move 6 from 4 to 1
+move 2 from 1 to 3
+move 22 from 8 to 6
+move 1 from 2 to 5
+move 3 from 7 to 8
+move 15 from 6 to 4
+move 7 from 3 to 7
+move 4 from 6 to 9
+move 2 from 9 to 2
+move 6 from 3 to 5
+move 3 from 9 to 5
+move 5 from 5 to 8
+move 1 from 2 to 1
+move 6 from 8 to 2
+move 1 from 1 to 2
+move 3 from 5 to 3
+move 1 from 7 to 2
+move 4 from 7 to 8
+move 4 from 6 to 1
+move 1 from 5 to 1
+move 4 from 8 to 7
+move 2 from 3 to 2
+move 1 from 1 to 3
+move 15 from 4 to 2
+move 3 from 7 to 3
+move 4 from 7 to 2
+move 1 from 4 to 9
+move 5 from 3 to 8
+move 29 from 2 to 1
+move 1 from 9 to 5
+move 1 from 2 to 1
+move 11 from 1 to 5
+move 1 from 4 to 5
+move 2 from 6 to 3
+move 1 from 3 to 4
+move 16 from 1 to 9
+move 4 from 8 to 4
+move 3 from 6 to 9
+move 1 from 3 to 7
+move 1 from 7 to 3
+move 6 from 1 to 6
+move 3 from 4 to 3
+move 3 from 8 to 5
+move 3 from 1 to 8
+move 3 from 1 to 4
+move 2 from 4 to 9
+move 3 from 6 to 3
+move 15 from 5 to 2
+move 3 from 2 to 3
+move 4 from 2 to 7
+move 2 from 5 to 9
+move 10 from 3 to 6
+move 11 from 9 to 5
+move 2 from 4 to 9
+move 8 from 9 to 4
+move 1 from 9 to 6
+move 7 from 4 to 6
+move 3 from 5 to 8
+move 22 from 6 to 9
+move 4 from 7 to 8
+move 8 from 5 to 8
+move 2 from 4 to 3
+move 1 from 8 to 1
+move 17 from 8 to 3
+move 3 from 3 to 4
+move 13 from 3 to 9
+move 20 from 9 to 7
+move 2 from 2 to 9
+move 19 from 9 to 5
+move 1 from 1 to 4
+move 3 from 2 to 7
+move 4 from 4 to 3
+move 1 from 9 to 8
+move 18 from 5 to 1
+move 1 from 9 to 4
+move 1 from 9 to 7
+move 2 from 4 to 8
+move 1 from 5 to 4
+move 3 from 2 to 7
+move 3 from 3 to 1
+move 2 from 1 to 3
+move 3 from 3 to 8
+move 1 from 4 to 8
+move 6 from 8 to 2
+move 1 from 3 to 9
+move 1 from 3 to 9
+move 10 from 1 to 9
+move 7 from 1 to 7
+move 4 from 7 to 4
+move 29 from 7 to 3
+move 6 from 2 to 9
+move 25 from 3 to 6
+move 5 from 3 to 9
+move 13 from 6 to 9
+move 12 from 6 to 2
+move 1 from 8 to 9
+move 10 from 2 to 6
+move 7 from 6 to 5
+move 20 from 9 to 3
+move 11 from 3 to 6
+move 1 from 7 to 9
+move 2 from 2 to 9
+move 19 from 9 to 2
+move 14 from 6 to 8
+move 4 from 5 to 2
+move 2 from 4 to 6
+move 3 from 5 to 1
+move 13 from 8 to 5
+move 1 from 6 to 1
+move 2 from 4 to 2
+move 8 from 2 to 4
+move 6 from 4 to 7
+move 1 from 9 to 8
+move 2 from 4 to 7
+move 5 from 2 to 4
+move 4 from 4 to 2
+move 10 from 5 to 6
+move 1 from 1 to 7
+move 1 from 5 to 4
+move 1 from 4 to 9
+move 4 from 7 to 8
+move 5 from 1 to 7
+move 1 from 9 to 7
+move 7 from 3 to 2
+move 2 from 5 to 2
+move 8 from 6 to 9
+move 1 from 4 to 6
+move 3 from 7 to 4
+move 5 from 9 to 7
+move 2 from 4 to 3
+move 20 from 2 to 4
+move 2 from 4 to 8
+move 14 from 4 to 2
+move 12 from 7 to 4
+move 8 from 2 to 1
+move 10 from 2 to 4
+move 6 from 8 to 5
+move 1 from 7 to 8
+move 4 from 4 to 3
+move 1 from 3 to 9
+move 1 from 2 to 7
+move 1 from 6 to 8
+move 5 from 3 to 5
+move 1 from 3 to 2
+move 7 from 4 to 5
+move 6 from 1 to 7
+move 5 from 7 to 6
+move 1 from 6 to 5
+move 2 from 7 to 8
+move 1 from 2 to 6
+move 2 from 8 to 2
+move 5 from 5 to 7
+move 6 from 6 to 8
+move 16 from 4 to 9
+move 16 from 9 to 4
+move 11 from 5 to 4
+move 5 from 8 to 3
+move 2 from 5 to 2
+move 14 from 4 to 2
+move 1 from 6 to 3
+move 1 from 6 to 9
+move 1 from 5 to 3
+move 3 from 8 to 2
+move 10 from 4 to 7
+move 5 from 9 to 2
+move 3 from 4 to 7
+move 1 from 1 to 4
+move 3 from 2 to 5
+move 2 from 3 to 7
+move 1 from 4 to 2
+move 18 from 2 to 8
+move 3 from 8 to 4
+move 5 from 3 to 1
+move 1 from 3 to 9
+move 1 from 9 to 3
+move 8 from 8 to 7
+move 2 from 5 to 4
+move 1 from 5 to 6
+move 1 from 2 to 5
+move 1 from 5 to 8
+move 1 from 6 to 9
+move 3 from 2 to 7
+move 27 from 7 to 4
+move 2 from 2 to 4
+move 4 from 8 to 4
+move 1 from 9 to 8
+move 3 from 1 to 6
+move 1 from 3 to 5
+move 3 from 8 to 3
+move 1 from 1 to 4
+move 1 from 8 to 1
+move 3 from 1 to 4
+move 2 from 8 to 2
+move 2 from 6 to 2
+move 8 from 4 to 9
+move 1 from 7 to 1
+move 1 from 5 to 4
+move 1 from 7 to 3
+move 4 from 2 to 7
+move 1 from 8 to 6
+move 8 from 9 to 7
+move 1 from 6 to 3
+move 3 from 3 to 4
+move 37 from 4 to 1
+move 1 from 4 to 5
+move 13 from 7 to 8
+move 6 from 8 to 4
+move 5 from 8 to 3
+move 1 from 7 to 6
+move 4 from 1 to 5
+move 1 from 6 to 5
+move 2 from 8 to 4
+move 32 from 1 to 5
+move 1 from 1 to 4
+move 5 from 3 to 5
+move 1 from 3 to 2
+move 1 from 2 to 9
+move 19 from 5 to 2
+move 1 from 9 to 1
+move 16 from 5 to 1
+move 7 from 5 to 6
+move 1 from 3 to 1
+move 11 from 1 to 2
+move 18 from 2 to 4
+move 1 from 5 to 9
+move 8 from 6 to 1
+move 10 from 2 to 6
+move 7 from 4 to 9
+move 2 from 2 to 1
+move 7 from 4 to 2
+move 5 from 4 to 5
+move 2 from 9 to 6
+move 9 from 6 to 3
+move 5 from 5 to 3
+move 8 from 4 to 9
+move 7 from 9 to 8
+move 4 from 2 to 9
+move 10 from 3 to 1
+move 6 from 8 to 1
+move 2 from 6 to 3
+move 5 from 3 to 8
+move 3 from 2 to 7
+move 1 from 9 to 5
+move 1 from 3 to 5
+move 2 from 7 to 8
+move 1 from 8 to 9
+move 1 from 6 to 1
+move 23 from 1 to 4
+move 2 from 5 to 3
+move 1 from 8 to 2
+move 2 from 8 to 5
+move 2 from 5 to 6
+move 1 from 2 to 7
+move 1 from 7 to 5
+move 4 from 9 to 7
+move 1 from 7 to 5
+move 1 from 3 to 6
+move 3 from 7 to 4
+move 1 from 3 to 8
+move 1 from 4 to 6
+move 6 from 1 to 8
+move 4 from 6 to 4
+move 2 from 9 to 1
+move 1 from 5 to 1
+move 19 from 4 to 2
+move 2 from 9 to 3
+move 1 from 9 to 3
+move 9 from 1 to 8
+move 1 from 5 to 8
+move 1 from 9 to 3
+move 2 from 3 to 9
+move 3 from 8 to 4
+move 1 from 4 to 9
+move 1 from 9 to 5
+move 2 from 3 to 4
+move 6 from 4 to 7
+move 3 from 9 to 5
+move 4 from 4 to 7
+move 1 from 5 to 6
+move 18 from 2 to 7
+move 13 from 7 to 9
+move 3 from 5 to 1
+move 1 from 2 to 1
+move 1 from 6 to 5
+move 3 from 1 to 7
+move 1 from 1 to 5
+move 7 from 9 to 6
+move 8 from 7 to 4
+move 11 from 7 to 6
+move 5 from 9 to 2
+move 17 from 6 to 1
+move 2 from 5 to 1
+move 11 from 8 to 1
+move 20 from 1 to 2
+move 3 from 8 to 1
+move 1 from 9 to 8
+move 1 from 6 to 1
+move 11 from 1 to 7
+move 18 from 2 to 3
+move 12 from 4 to 8
+move 11 from 7 to 3
+move 7 from 2 to 3
+move 2 from 1 to 5
+move 1 from 1 to 3
+move 1 from 8 to 1
+move 1 from 5 to 9
+move 1 from 9 to 6
+move 1 from 8 to 7
+move 1 from 5 to 3
+move 1 from 6 to 7
+move 2 from 8 to 1
+move 8 from 3 to 2
+move 7 from 2 to 9
+move 6 from 8 to 6
+move 1 from 9 to 3
+move 2 from 6 to 4
+move 5 from 9 to 6
+move 7 from 6 to 2
+move 8 from 2 to 9
+move 2 from 1 to 9
+move 2 from 7 to 2
+move 2 from 4 to 8
+move 1 from 2 to 7
+move 25 from 3 to 7
+move 7 from 9 to 7
+move 1 from 2 to 5
+move 1 from 1 to 4
+move 3 from 8 to 1
+move 3 from 1 to 8
+move 3 from 7 to 8
+move 15 from 7 to 3
+move 10 from 8 to 3
+move 1 from 5 to 7
+move 1 from 8 to 5
+move 3 from 9 to 2
+move 1 from 6 to 4
+move 2 from 2 to 7
+move 1 from 2 to 5
+move 14 from 7 to 9
+move 1 from 6 to 2
+move 1 from 7 to 1
+move 1 from 5 to 4
+move 3 from 4 to 3
+move 1 from 7 to 6
+move 1 from 2 to 7
+move 1 from 1 to 2
+move 3 from 9 to 1
+move 1 from 6 to 2
+move 2 from 2 to 6
+move 17 from 3 to 6
+move 1 from 8 to 3
+move 1 from 5 to 4
+move 2 from 7 to 2
+move 9 from 9 to 8
+move 1 from 9 to 3
+move 16 from 3 to 2
+move 1 from 7 to 5
+move 5 from 6 to 5
+move 1 from 1 to 6
+move 1 from 4 to 1
+move 1 from 9 to 3
+move 9 from 8 to 6
+move 3 from 1 to 5
+move 1 from 9 to 1
+move 16 from 2 to 1
+move 2 from 2 to 7
+move 2 from 3 to 9
+move 2 from 7 to 4
+move 2 from 9 to 3
+move 3 from 3 to 5
+move 1 from 4 to 5
+move 1 from 4 to 2
+move 1 from 1 to 7
+move 1 from 7 to 1
+move 1 from 3 to 6
+move 2 from 5 to 1
+move 3 from 6 to 2
+move 2 from 5 to 8
+move 8 from 5 to 4
+move 1 from 5 to 3
+move 1 from 3 to 2
+move 1 from 8 to 3
+move 1 from 3 to 8
+move 4 from 1 to 7
+move 9 from 1 to 7
+move 6 from 1 to 8
+move 3 from 7 to 4
+move 7 from 6 to 7
+move 11 from 4 to 3
+move 2 from 3 to 8
+move 8 from 3 to 8
+move 4 from 6 to 1
+move 1 from 7 to 4
+move 2 from 1 to 2
+move 8 from 7 to 2
+move 1 from 4 to 8
+move 10 from 8 to 2
+move 2 from 6 to 1
+move 1 from 1 to 4
+move 1 from 4 to 8
+move 2 from 1 to 4
+move 6 from 6 to 5
+move 1 from 1 to 9
+move 2 from 6 to 8
+move 1 from 4 to 5
+move 1 from 6 to 9
+move 4 from 8 to 9
+move 1 from 7 to 1
+move 6 from 8 to 6
+move 1 from 6 to 1
+move 1 from 4 to 9
+move 2 from 9 to 5
+move 5 from 5 to 9
+move 8 from 9 to 5
+move 2 from 8 to 5
+move 3 from 6 to 9
+move 8 from 5 to 7
+move 5 from 5 to 6
+move 1 from 9 to 2
+move 1 from 3 to 1
+move 1 from 6 to 7
+move 1 from 5 to 6
+move 24 from 2 to 4
+move 3 from 9 to 7
+move 16 from 4 to 5
+move 2 from 1 to 3
+move 12 from 5 to 6
+move 1 from 9 to 5
+move 4 from 5 to 9
+move 1 from 1 to 6
+move 1 from 5 to 2
+move 2 from 9 to 8
+move 1 from 8 to 1
+move 5 from 4 to 5
+move 2 from 3 to 5
+move 1 from 8 to 3
+move 1 from 1 to 6
+move 3 from 5 to 7
+move 1 from 9 to 1
+move 1 from 2 to 8";
+
+// puzzleInput = @"    [D]    
+// [N] [C]    
+// [Z] [M] [P]
+//  1   2   3 
+
+// move 1 from 2 to 1
+// move 3 from 1 to 3
+// move 2 from 2 to 1
+// move 1 from 1 to 2";
+
+var puzzleInputs = puzzleInput.Split("\r\n");
+
+var stacks = new List<string>();
+var movements = new List<string>();
+
+var breakFound = false;
+
+for (int i = 0; i < puzzleInputs.Length; i++)
+{
+    // process every line in the input
+    var input = puzzleInputs[i];
+
+    // each section is separated by an empty line
+    if (input.Length == 0)
+    {
+        breakFound = true;
+        continue;
+    }
+
+    if (breakFound == false)
+    {
+        // the first section is the stack/crate input
+        stacks.Add(input);
+    }
+    else
+    {
+        // the second half is the movement input
+        movements.Add(input);
+    }
+}
+
+var listStackPositions = new List<int>();
+
+// final item has the stack number - use the position of the stack numbers to indicate the position of the Crate values per row
+var stackPositions = stacks.Last();
+var stackCrates = new Dictionary<int, Stack<string>>();
+var stackCratesPart2 = new Dictionary<int, Stack<string>>();
+
+for (int i = 0; i < stackPositions.Length; i++)
+{
+    if (int.TryParse(stackPositions.Substring(i, 1), out int stackNumber))
+    {
+        for (int j = stacks.Count - 2; j >= 0; j--)
+        {
+            var crate = stacks[j].Substring(i, 1);
+
+            if (!stackCrates.ContainsKey(stackNumber))
+            {
+                // add a new stack
+                stackCrates.Add(stackNumber, new Stack<string>());
+                stackCratesPart2.Add(stackNumber, new Stack<string>());
+            }
+
+            if (!string.IsNullOrWhiteSpace(crate))
+            {
+                // push the crate onto the stack
+                stackCrates.Single(stack => stack.Key == stackNumber).Value.Push(crate);
+                stackCratesPart2.Single(stack => stack.Key == stackNumber).Value.Push(crate);
+            }
+        }
+    }
+}
+
+foreach (var movement in movements)
+{
+    // move n from x to y
+    var movementParts = movement.Split(" ");
+    
+    var numberOfCrates = int.Parse(movementParts[1]);
+    var fromStackNumber = int.Parse(movementParts[3]);
+    var toStackNumber = int.Parse(movementParts[5]);
+    
+    var stackFrom = stackCrates.Single(stack => stack.Key == fromStackNumber).Value;
+    var stackTo = stackCrates.Single(stack => stack.Key == toStackNumber).Value;
+
+    var stackFromPart2 = stackCratesPart2.Single(stack => stack.Key == fromStackNumber).Value;
+    var stackToPart2 = stackCratesPart2.Single(stack => stack.Key == toStackNumber).Value;
+    
+    var interimStack = new Stack<string>();
+    for (int i = 0; i < numberOfCrates; i++)
+    {
+        // step 1 - acts like a stack
+        stackTo.Push(stackFrom.Pop());
+
+        // step 2 - acts like a queue, emulate one by pushing to an interim stack
+        interimStack.Push(stackFromPart2.Pop());
+    }
+    foreach (var interim in interimStack)
+    {
+        stackToPart2.Push(interim);
+    }
+}
+
+// peek at the top item in each stack
+var total = string.Empty;
+foreach (var stack in stackCrates)
+{
+    total += stack.Value.Pop();
+}
+Console.WriteLine(total);
+
+var totalPart2 = string.Empty;
+foreach (var stack in stackCratesPart2)
+{
+    totalPart2 += stack.Value.Pop();
+}
+Console.WriteLine(totalPart2);
